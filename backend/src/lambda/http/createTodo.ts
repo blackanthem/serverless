95cb848f340b373ba8757af70acb38da6f56cb35
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-07-22 13:57:08 +0000

    create todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,21 +1,28 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
+import { getUserId } from '../utils'
 import { createTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
 
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
+const logger = createLogger('getTodos')
 
-    return undefined
-)
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info('Handle Create Todo request', { event })
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+  const userId = getUserId(event)
+  const todo: CreateTodoRequest = JSON.parse(event.body)
+
+  const item = await createTodo(userId, todo)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(item)
+  }
+}
